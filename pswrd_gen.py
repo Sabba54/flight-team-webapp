2e3b2ab49fbaf9630f5ac5120f94ec8146f7a857
Sabba54 <115191070+Sabba54@users.noreply.github.com>
2022-10-06 23:56:30 +0200

    Update pswrd_gen.py

--- pswrd_gen.py
+++ pswrd_gen.py
@@ -4,7 +4,7 @@ import streamlit_authenticator as stauth
 
 teams = ['CAD','SOFTWARE','HARDWARE','GESTIONE']
 usernames = ['CAD_team','SFTW_team','HRDW_team','ADMIN']
-passwords = ['Cad2023','Sftw2023','Hrdw2023','admin2023']
+passwords = ['Cad2023','Sftw202333','Hrdw2023','admin2023']
 
 hashed_password = stauth.Hasher(passwords).generate()
 
