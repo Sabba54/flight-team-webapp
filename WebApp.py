b77cfc97ae5997dc0f57375383c2a855dc4a401c
Sabba54 <115191070+Sabba54@users.noreply.github.com>
2022-10-07 15:59:01 +0200

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -50,8 +50,13 @@ def send_email():
 
 
     server = smtplib.SMTP('smtp.gmail.com',587)
+    server.connect("smtp.gmail.com",587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
     server.login(from_addr,'Sapienza1834805')
     server.send_message(msg,from_addr=from_addr,to_addrs=[to_addr])
+    server.quit()
 
 
 
